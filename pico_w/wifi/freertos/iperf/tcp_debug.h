b15c5ecb9ff0c85f3abbe2868ba8c1b0b201886f
Edward Vidal Jr <develone@sbcglobal.net>
2023-03-31 06:54:25 -0600

    tcp_server_accept was not prototyped in tcp_debug.h void tcp_server_err(void *arg, err_t err); was declared twice.

--- pico_w/wifi/freertos/iperf/tcp_debug.h
+++ pico_w/wifi/freertos/iperf/tcp_debug.h
@@ -32,7 +32,7 @@ err_t tcp_server_send_data(void *arg, struct tcp_pcb *tpcb);
 err_t tcp_server_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err);
 err_t tcp_server_poll(void *arg, struct tcp_pcb *tpcb);
 void tcp_server_err(void *arg, err_t err);
-void tcp_server_err(void *arg, err_t err);
+err_t tcp_server_accept(void *arg, struct tcp_pcb *client_pcb, err_t err);
 bool tcp_server_open(void *arg);
 
 #endif
