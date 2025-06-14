d667e852183b6fd30c569567ee9ad6482c06f6a6
Edward Vidal Jr <develone@sbcglobal.net>
2025-06-14 08:08:52 -0600

    testing pi4-30  code on pi5-70 using export WIFI_SSID="ATTtpHTfPi_Guest"

--- 3remotes.sh
+++ 3remotes.sh
@@ -10,7 +10,7 @@ cd remote7
 
 cp ../exe-ocd.sh .
 
-cmake -DPICO_BOARD=pico_w  -DHOSTNAME="remote7" -DWIFI_SSID="WIFI_SSID" -DWIFI_PASSWORD="WIFI_PASSWORD"   ..
+cmake -DPICO_BOARD=pico_w  -DHOSTNAME="remote7" -DWIFI_SSID="WIFI_SSID" -DWIFI_PASSWORD="WIFI_PASSWORD"  ..
 
 
 make
