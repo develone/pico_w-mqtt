42374f746692ea65b7ee36ed3e77ee9993b0d1d2
Edward Vidal Jr <develone@sbcglobal.net>
2025-06-14 18:46:14 -0600

    Welcome to minicom 2.8
    OPTIONS: I18n
    Port /dev/ttyUSB0, 18:32:47
    
    Press CTRL-A Z for help on special keys
    Starting FreeRTOS on core 0: ver 0.0.11 remote3
    Connecting to Wi-Fi...
    Connected.
    0x200014fc 0x200014cc 0x200016cb 0x200014cc
    Connected. iperf server 192.168.1.176 4001
    0x20001528 0x200014cc 0x200016cb 0x200014cc
    mqtt_ip = 0x5d01a8c0 mqtt_port = 1883
    MQTT client "remote3" connection cb: status 0
    MQTT client "remote3" request cb: err 0
    MQTT client "remote3" request cb: err 0
    Starting server at 192.168.1.176 on port 4001
    open_task starts
    close_task starts
    close_task close_flg 0
    open_task open_flg 0
    0x2000154f 0x200014cc 0x200016cb 0x200014cc
    batt task 0x046 -> 0.056396 V
    ntp address 66.118.230.14
    got ntp response: 15/06/2025 00:38:29
    
    devel@pi5-80:~/pico_w-mqtt/pi_tcp_tests $ ./cli1
    Socket created successfully
    Connected with server successfully
    Starting FreeRTOS on core 0: ver 0.0.11 remote1 Connected. iperf server 192.168.1.178 4001  mqtt_ip = 0x5d01a8c0 mqtt_port = 1883  batt task 0x04d -> 0.062036 V 15/06/2025 00:38:04 batt task 0x04e -> 0.062842 V 15/06/2025 00:38:35 batt task 0x04e -> 0.062842 VAlarm Fired Sunday 15 June 0:39:04 2025  15/06/2025 00:39:06 batt task 0x04d -> 0.062036 Vbatt task 0x04e -> 0.062842 V 15/0!
    devel@pi5-80:~/pico_w-mqtt/pi_tcp_tests $ ./cli2
    Socket created successfully
    Connected with server successfully
    Starting FreeRTOS on core 0: ver 0.0.11 remote2 Connected. iperf server 192.168.1.177 4001  mqtt_ip = 0x5d01a8c0 mqtt_port = 1883  batt task 0x041 -> 0.052368 V 15/06/2025 00:37:55 batt task 0x042 -> 0.053174 V 15/06/2025 00:38:26 batt task 0x042 -> 0.053174 VAlarm Fired Sunday 15 June 0:38:55 2025  15/06/2025 00:38:57 batt task 0x042 -> 0.053174 Vbatt task 0x042 -> 0.053174 V 15/00
    devel@pi5-80:~/pico_w-mqtt/pi_tcp_tests $ ./cli3
    Socket created successfully
    Connected with server successfully
    Starting FreeRTOS on core 0: ver 0.0.11 remote3 Connected. iperf server 192.168.1.176 4001  mqtt_ip = 0x5d01a8c0 mqtt_port = 1883  batt task 0x046 -> 0.056396 V 15/06/2025 00:38:29 batt task 0x045 -> 0.055591 V 15/06/2025 00:38:59 batt task 0x046 -> 0.056396 VAlarm Fired Sunday 15 June 0:39:29 2025  15/06/2025 00:39:30 batt task 0x045 -> 0.055591 Vbatt task 0x046 -> 0.056396 V 15/0
    
    devel@pi5-80:~/pico_w-mqtt/pi_tcp_tests $ iperf -c remote1
    ------------------------------------------------------------
    Client connecting to remote1, TCP port 5001
    TCP window size: 16.0 KByte (default)
    ------------------------------------------------------------
    [  1] local 192.168.1.240 port 42118 connected with 192.168.1.178 port 5001 (icwnd/mss/irtt=14/1460/190946)
    [ ID] Interval       Transfer     Bandwidth
    [  1] 0.0000-10.1073 sec  18.1 MBytes  15.0 Mbits/sec
    devel@pi5-80:~/pico_w-mqtt/pi_tcp_tests $ iperf -c remote2
    ------------------------------------------------------------
    Client connecting to remote2, TCP port 5001
    TCP window size: 16.0 KByte (default)
    ------------------------------------------------------------
    [  1] local 192.168.1.240 port 59054 connected with 192.168.1.177 port 5001 (icwnd/mss/irtt=14/1460/227414)
    [ ID] Interval       Transfer     Bandwidth
    [  1] 0.0000-10.0627 sec  18.3 MBytes  15.2 Mbits/sec
    devel@pi5-80:~/pico_w-mqtt/pi_tcp_tests $ iperf -c remote3
    ------------------------------------------------------------
    Client connecting to remote3, TCP port 5001
    TCP window size: 16.0 KByte (default)
    ------------------------------------------------------------
    [  1] local 192.168.1.240 port 44390 connected with 192.168.1.176 port 5001 (icwnd/mss/irtt=14/1460/115658)
    [ ID] Interval       Transfer     Bandwidth
    [  1] 0.0000-10.1105 sec  16.6 MBytes  13.8 Mbits/sec

--- pico_w/wifi/freertos/iperf/head_tail.h
+++ pico_w/wifi/freertos/iperf/head_tail.h
@@ -1,22 +1,17 @@
 #ifndef HEAD_TAIL_H
 #define HEAD_TAIL_H
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "pico/cyw43_arch.h"
-
-#include "lwip/pbuf.h"
-#include "lwip/tcp.h"
-char tmp[80];
+ 
+/*
 char * head;
 char * tail;
 char * endofbuf;
 char * topofbuf;
+*/
 
-
-char * bump_head(char * head, char * endofbuf,char * topofbuf);
-char * bump_tail(char * tail,char * endofbuf,char * topofbuf);
-char * dec_head(char * head,char * endofbuf,char * topofbuf);
-char * dec_tail(char * tail,char * endofbuf,char * topofbuf);
-char * head_tail_helper(char * head, char * tail,char * endofbuf,char * topofbuf, char * inpstr);
+char * bump_head(char * ptrhead, char * ptrendofbuf,char * ptrtopofbuf);
+char * bump_tail(char * ptrtail,char * ptrendofbuf,char * ptrtopofbuf);
+char * dec_head(char * ptrhead,char * ptrendofbuf,char * ptrtopofbuf);
+char * dec_tail(char * ptrtail,char * ptrendofbuf,char * ptrtopofbuf);
+char * head_tail_helper(char * ptrhead, char * ptrtail,char * ptrendofbuf,char * ptrtopofbuf, char * inpstr);
 
 #endif
